c18f4276d07297ec1e6d4db0a1a88e90bcc1835f
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-02 21:43:46 +0200

    checks uppercase letters

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    result = ""
     for char in str:
         ascii_val = ord(char)
         if ascii_val >= 97 and ascii_val <= 122:
             ascii_val -= 32
-        print(chr(ascii_val), end="")
-    print()
+        result += chr(ascii_val)
+    print(f"{result}\n", end="")
+
