50966b6685857401d55f272537b596fd2a7b3042
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-13 14:44:37 +0200

    adding error conditions

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    this class represents a square
+    """
+
+    def __init__(self, size=0):
+        """
+        initializes a Square instance
+
+        args:
+            size (int): the size of the square. Defaults to 0
+
+        Raises:
+            typeError: if the size is not an integer
+            valueError: if the size is less than 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
