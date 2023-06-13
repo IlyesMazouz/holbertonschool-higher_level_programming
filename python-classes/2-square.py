f07861ed7690cf6562c58e77dc186abf209a9d27
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-13 14:52:59 +0200

    adding error conditions

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,6 +3,7 @@
 this module defines a class that represents a square
 """
 
+
 class Square:
     """
     this class represents a square
@@ -12,15 +13,17 @@ class Square:
         """
         initializes a Square instance
 
-        args:
-            size (int): the size of the square. Defaults to 0
+        Args:
+            size (int): The size of the square. Defaults to 0
 
         Raises:
-            typeError: if the size is not an integer
-            valueError: if the size is less than 0
+            TypeError: If the size is not an integer
+            ValueError: If the size is less than 0
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
