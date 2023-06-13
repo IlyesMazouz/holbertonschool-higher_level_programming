a90226c11172c9aec04d6e7f3d7bff778ae44fc2
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-13 22:53:33 +0200

    square of size

---
+++ python-classes/3-square.py
@@ -0,0 +1,35 @@
+class Square:
+    """
+    this class defines a square
+
+    Attributes:
+        __size (int): The size of the square
+
+    """
+
+    def __init__(self, size=0):
+        """
+        initializes a square instance
+
+        Args:
+            size (int): The size of the square. Defaults to 0
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        calculates the area of the square
+        Returns:
+            int: The area of the square.
+
+        """
+        return self.__size ** 2
