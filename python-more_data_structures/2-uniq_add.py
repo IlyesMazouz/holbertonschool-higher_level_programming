112d7abd624ce4cd8a05391b98842fb96e889232
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-16 13:32:29 +0200

    sum of integers

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    unique_set = set()
+
+    for num in my_list:
+        unique_set.add(num)
+
+    sum_of_unique = sum(unique_set)
+
+    return sum_of_unique
