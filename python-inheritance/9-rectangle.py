6e0d0b83a2789dacc1725c9c013535f032a8674c
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-23 14:00:18 +0200

    test

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -55,3 +55,4 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """Return a string representation of the rectangle"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
