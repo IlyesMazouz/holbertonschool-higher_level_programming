a961c7f42a52db814b6ab362475985be05dafe56
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-27 23:47:22 +0200

    the unittest for rectangle class

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-"""
-unittest for rectangle.py
-"""
+import io
 import unittest
+from contextlib import redirect_stdout
 from models.rectangle import Rectangle
 
 
@@ -11,35 +9,66 @@ class TestRectangle(unittest.TestCase):
     Test cases for the Rectangle class
     """
 
-    def test_init_with_id(self):
+    def setUp(self):
         """
-        Test the initialization of Rectangle with an explicit id
+        Set up a Rectangle instance before each test method
         """
-        r = Rectangle(2, 4, 1, 1, 5)
-        self.assertEqual(r.id, 5)
-        self.assertEqual(r.width, 2)
-        self.assertEqual(r.height, 4)
-        self.assertEqual(r.x, 1)
-        self.assertEqual(r.y, 1)
+        self.rect = Rectangle(4, 5, 1, 2, 10)
 
-    def test_init_without_id(self):
+    def test_attributes(self):
         """
-        Test the initialization of Rectangle without an explicit id
+        Test the attributes of the Rectangle instance
         """
-        Rectangle._Base__nb_objects = 0  # Reset the nb_objects count
-        r1 = Rectangle(3, 5, 2, 2)
-        r2 = Rectangle(4, 6)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r1.width, 3)
-        self.assertEqual(r1.height, 5)
-        self.assertEqual(r1.x, 2)
-        self.assertEqual(r1.y, 2)
-        self.assertEqual(r2.width, 4)
-        self.assertEqual(r2.height, 6)
-        self.assertEqual(r2.x, 0)
-        self.assertEqual(r2.y, 0)
+        self.assertEqual(self.rect.width, 4)
+        self.assertEqual(self.rect.height, 5)
+        self.assertEqual(self.rect.x, 1)
+        self.assertEqual(self.rect.y, 2)
+        self.assertEqual(self.rect.id, 10)
+
+    def test_area(self):
+        """
+        Test the area() method of the Rectangle instance
+        """
+        self.assertEqual(self.rect.area(), 20)
+
+    def test_str(self):
+        """
+        Test the __str__() method of the Rectangle instance
+        """
+        expected = "[Rectangle] (10) 1/2 - 4/5"
+        self.assertEqual(str(self.rect), expected)
+
+    def test_display(self):
+        """
+        Test the display() method of the Rectangle instance
+        """
+        expected_output = "\n\n ####\n ####\n ####\n ####\n ####\n"
+        with io.StringIO() as buf, redirect_stdout(buf):
+            self.rect.display()
+            output = buf.getvalue()
+        self.assertEqual(output, expected_output)
+
+    def test_update_args(self):
+        """
+        Test the update() method of the Rectangle instance with *args
+        """
+        self.rect.update(20, 6, 7, 3, 4)
+        self.assertEqual(self.rect.id, 20)
+        self.assertEqual(self.rect.width, 6)
+        self.assertEqual(self.rect.height, 7)
+        self.assertEqual(self.rect.x, 3)
+        self.assertEqual(self.rect.y, 4)
+
+    def test_update_kwargs(self):
+        """
+        Test the update() method of the Rectangle instance with **kwargs
+        """
+        self.rect.update(width=8, height=9, x=5, y=6)
+        self.assertEqual(self.rect.width, 8)
+        self.assertEqual(self.rect.height, 9)
+        self.assertEqual(self.rect.x, 5)
+        self.assertEqual(self.rect.y, 6)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
