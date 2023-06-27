03730ae5a7e688dc379aea75403e77196c170d72
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-27 23:04:56 +0200

    outputing the attribute values

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,9 @@ class Rectangle(Base):
         """Prints the Rectangle instance with the character '#'"""
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Returns a string representation of the Rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height
+        )
