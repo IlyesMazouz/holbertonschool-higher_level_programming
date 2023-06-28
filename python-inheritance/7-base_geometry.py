9e17346942b8977c0bb1a453f16a113877c2f87b
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-06-28 20:49:33 +0200

    inheriting 6-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class representing a base geometry shape.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry shape.
+        This method should be overridden by subclasses.
+        :return: None
+        :raises: Exception with the message "area() is not implemented"
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate the given value as an integer based on the provided rules.
+        :param name: The name of the value as a string.
+        :param value: The value to be validated.
+        :return: None
+        :raises:
+            - TypeError if value is not an integer
+              with the message "<name> must be an integer"
+            - ValueError if value is less than or equal to 0
+              with the message "<name> must be greater than 0"
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
