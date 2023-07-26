43e79b361c1b1c6c9786868e575b497060c291d7
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-07-26 20:12:27 +0200

    listing all records of second table

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- This SQL script lists all records of the 'second_table' from the database 'hbtn_0c_0',
+-- displaying both the score and name, ordered by score (top first)
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
