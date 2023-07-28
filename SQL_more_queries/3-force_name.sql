3946ad15c075ddd2f4e09822f07b8f4802ebaf84
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-07-28 10:15:05 +0200

    creating a table

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,7 @@
--- This SQL script creates the 'force_name' table on the MySQL server.
--- The table has an 'id' column of type INT and a 'name' column of type VARCHAR(256) which cannot be null.
+-- This SQL script creates the 'force_name' table on the MySQL server
+-- The table has an 'id' column of type INT and a 'name' column of type VARCHAR(256) which cannot be null
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.force_name (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
