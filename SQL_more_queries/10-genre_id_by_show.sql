ce338ad8552f30441012268aac82c2252a92cfae
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-07-28 13:06:34 +0200

    listing all shows

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+-- This SQL script lists all shows contained in the 'hbtn_0d_tvshows' database that have at least one genre linked
+-- Each record displays: tv_shows.title - tv_show_genres.genre_id
+-- The results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- The script uses a JOIN operation to combine data from both the 'tv_shows' and 'tv_show_genres' tables
+
+-- Listing shows with their linked genre_ids
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
