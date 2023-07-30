77d34f795843f075052651507303411567410515
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-07-30 13:04:49 +0200

    reading and printing txt file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,12 +2,14 @@
 """
 a function that reads a text file (UTF8) and prints it to stdout
 """
+
+
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints its content to stdout
 
     filename (str): The name of the file to read
     """
-    with open(filename, mode='r', encoding='utf-8') as file:
+    with open(filename, mode="r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")
