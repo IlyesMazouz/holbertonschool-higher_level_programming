c7acd90fb7ba698373e3912b0067c42397510212
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-07-30 20:33:46 +0200

    adding python arguments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
+
 """
- a script that adds all arguments to a Python list,
- and then save them to a file
+a script that adds all arguments to a Python list, and then save them to a file
 """
 
 import sys
-from os import path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__(
+    '6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    """Load existing data from the file or
-      create an empty list if the file doesn't exist"""
-    if path.exists("add_item.json"):
-        my_list = load_from_json_file("add_item.json")
+file_name = "add_item.json"
+
+with open(file_name, 'a') as file:
+    """
+    If the file is not empty, load its content and append the new content to it
+    """
+    if (os.path.getsize(file_name) != 0):
+        items = load_from_json_file(file_name)
+        items += sys.argv[1:]
+        save_to_json_file(items, file_name)
+    """
+    If the file is empty, just append the new content
+    """
     else:
-        my_list = []
-
-    """Add all arguments to the list
-    """
-    my_list.extend(sys.argv[1:])
-
-    """ Save the updated list to the file
-    """
-    save_to_json_file(my_list, "add_item.json")
+        items = []
+        items += sys.argv[1:]
+        save_to_json_file(items, file_name)
