39d05e9a533698e50d0d8ea78bc38c15eab5cf0a
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-07-31 13:35:42 +0200

    listing all states starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-"""
-Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-"""
+"""Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
@@ -10,11 +8,7 @@ import sys
 
 def filter_states_by_name_starting_with_N(username, password, database):
     conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        host="localhost", port=3306, user=username, passwd=password, db=database
     )
 
     cursor = conn.cursor()
@@ -31,7 +25,11 @@ def filter_states_by_name_starting_with_N(username, password, database):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        print(
+            "Usage: {} <mysql_username> <mysql_password> <database_name>".format(
+                sys.argv[0]
+            )
+        )
         sys.exit(1)
 
     mysql_username = sys.argv[1]
