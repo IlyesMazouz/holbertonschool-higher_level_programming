e7fc17375fa63aa86b868b42a3701ad845f37cbd
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-07-31 14:25:35 +0200

    linking class to database

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-
 """
-a python file that contains the class definition of a State and an instance Base = declarative_base()
+a python file that contains the class definition of a State and an instance Base = declarative_base():
+
 """
 import sys
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
 
 class State(Base):
     __tablename__ = "states"
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+
+    from model_state import State
+
+    Base.metadata.create_all(engine)
