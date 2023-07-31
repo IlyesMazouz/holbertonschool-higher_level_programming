5f58506eb046def1bc19b0d69633618e2bbed8d4
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-07-31 14:12:15 +0200

    listing specific cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,34 @@
 #!/usr/bin/python3
 
-"""a script that takes in the name of a state as an argument 
-and lists all cities of that state, using the database hbtn_0e_4_usa"""
+"""Lists all cities of a given state from the database hbtn_0e_4_usa"""
 
 import MySQLdb
 import sys
 
 
-def list_cities_by_state(username, password, database):
+def filter_cities_by_state(username, password, database, state_name):
     conn = MySQLdb.connect(
         host="localhost", port=3306, user=username, passwd=password, db=database
     )
 
     cursor = conn.cursor()
 
-    cursor.execute(
-        """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        INNER JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC
-    """
-    )
+    query = "SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
+
+    cursor.execute(query, (state_name,))
 
     results = cursor.fetchall()
-    for row in results:
-        print(row)
+    city_names = ", ".join(city[0] for city in results)
+    print(city_names)
 
     cursor.close()
     conn.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print(
-            "Usage: {} <mysql_username> <mysql_password> <database_name>".format(
+            "Usage: {} <mysql_username> <mysql_password> <database_name> <state_name>".format(
                 sys.argv[0]
             )
         )
@@ -43,5 +37,6 @@ if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    list_cities_by_state(mysql_username, mysql_password, database_name)
+    filter_cities_by_state(mysql_username, mysql_password, database_name, state_name)
