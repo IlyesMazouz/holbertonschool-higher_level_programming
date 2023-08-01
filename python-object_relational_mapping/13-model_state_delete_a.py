d008e2ddd917659ec92960cf7a19d5cfd68b7896
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-01 14:26:35 +0200

    deleting all states with a

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,19 +11,26 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        print(
+            "Usage: {} <mysql_username> <mysql_password> <database_name>".format(
+                sys.argv[0]
+            )
+        )
         sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
 
     Base.metadata.bind = engine
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states_to_delete = session.query(State).filter(State.name.like("%a%")).all()
 
     for state in states_to_delete:
         session.delete(state)
