c4d38fb93f8eedc3dec65cd625195d99a5bdf41f
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-04 11:32:10 +0200

    write name function

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a function that prints My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError("first_name must be a string or last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
+
+
+if __name__ == "__main__":
+    say_my_name("John", "Smith")
+    say_my_name("Walter", "White")
+    say_my_name("Bob")
+    try:
+        say_my_name(12, "White")
+    except Exception as e:
+        print(e)
