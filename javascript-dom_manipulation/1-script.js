6b6f8a4aac68ee2e135070693be7c71c1c81d047
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-13 18:12:37 +0200

    switching colors upon click

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var redHeaderElement = document.getElementById('red_header');
-	
+
 	redHeaderElement.addEventListener('click', function () {
-	  var headerElement = document.querySelector('header');
-  
-	  headerElement.style.color = '#FF0000';
+		var headerElement = document.querySelector('header');
+
+		headerElement.style.color = 'red';
 	});
-  });
-  
+});
